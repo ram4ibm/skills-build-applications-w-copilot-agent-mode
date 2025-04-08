0c85ee5c9836228d3ca9056a120dd6ce7961c462
ram4ibm <ram4ibm@gmail.com>
2025-04-08 21:03:45 +0000

    updates with copilot code

--- octofit-tracker/backend/octofit_tracker/admin.py
+++ octofit-tracker/backend/octofit_tracker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Team, Activity, Leaderboard, Workout
+from .models import User, Team, Activity, Leaderboard, Workout # type: ignore
 
 # Register models in the admin interface
 class UserAdmin(admin.ModelAdmin):
